d96c69bb19569ade1487ec785ecf2e707083f735
qbu7 <qbu7@gatech.edu>
2019-04-13 09:57:07 +0800

    Clean up scripts

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import logging
+import sys
 import warnings
 
 import numpy as np
@@ -6,7 +7,8 @@ import pandas as pd
 
 from constants import *
 from get_data import get_data
-from util import feature_selection, visual_data_prep, append_existing_data
+from util import feature_selection, visual_data_prep, append_existing_data, \
+    multi_model_analysis
 
 
 def initial_setup():
@@ -19,11 +21,13 @@ def initial_setup():
     logger.info("Logger ready")
 
 
-def main(mode='Visual'):
+def main():
     initial_setup()
-    print("Accepted mode: \n1. Perform Analysis.\n2. Generate output file for visualization")
+    print("Accepted mode: \n1. Perform top feature selection Analysis.\n"
+          "2. Generate output file for visualization.\n"
+          "3. Run multiple models comparision analysis.\n")
     try:
-        mode = int(input('Enter the number of your selected mode:'))
+        mode = int(input('Enter the number of your selected mode:\n'))
     except ValueError:
         print("Not a number")
 
@@ -34,10 +38,11 @@ def main(mode='Visual'):
     logging.info("Get formatted pivot table data by city and census track from file: " + path)
 
     if mode == 1:
-        print("You have chose to run analysis for health care data")
+        print("You have chose to run top feature selection analysis for health care data")
+        top_n = int(input('Enter the number of top features you want to select:\n'))
 
         logging.info("About to run feature selection for city level data")
-        feature_selection_df = feature_selection(city_pv, outcome_cols, 5)
+        feature_selection_df = feature_selection(city_pv, outcome_cols, top_n)
         feature_selection_df.to_csv("city_data.csv")
         logging.info("Country data has been saved to csv")
 
@@ -47,7 +52,7 @@ def main(mode='Visual'):
         for state in states:
             logging.info("Feature selection for state %s" % state)
             state_data = tract_pv[tract_pv['StateDesc'] == state]
-            df = feature_selection(state_data, outcome_cols, 5)
+            df = feature_selection(state_data, outcome_cols, top_n)
             df["state"] = state
             state_feature_selection_df = pd.concat([state_feature_selection_df, df])
         state_feature_selection_df.to_csv("state_feature_selection.csv")
@@ -59,9 +64,11 @@ def main(mode='Visual'):
         logging.info("Start data prep in US level")
         prev_data = data[data['GeographicLevel'] == 'US']
 
-        df = visual_data_prep(tract_pv, prev_data, outcome_cols, 5)
+        df = visual_data_prep(city_pv, prev_data, outcome_cols, 5)
+        # df = visual_data_prep(tract_pv, prev_data, outcome_cols, 5)
         df['Region'] = 'US'
         df['Level'] = 'US'
+        df['PreventionCalculationLevel'] = 'National'
         result = pd.concat([result, df])
         logging.info("Complete data prep in US level")
 
@@ -83,115 +90,61 @@ def main(mode='Visual'):
                 state_df = pd.concat([state_df, df])
 
             # Append national level data for each state with state prevalence information
-            df = append_existing_data(result[result.Level == 'US'], prev_data, state )
+            df = append_existing_data(result[result.Level == 'US'], prev_data)
+            df["Region"] = state
+            df['Level'] = 'State'
+            df['PreventionCalculationLevel'] = 'National'
+            state_df = pd.concat([state_df, df])
 
             # For each cities with in the state
             cities = np.unique(state_data['CityName'])
-            us_df = result[result.Level == 'US']
+            national_calc_df = state_df[state_df.PreventionCalculationLevel == 'National']
+            national_calc_df = national_calc_df[national_calc_df.Region == state]
+            state_calc_df = state_df[state_df.PreventionCalculationLevel == 'State']
+            state_calc_df = state_calc_df[state_calc_df.Region == state]
 
             city_df = pd.DataFrame()
             for city in cities:
-                temp = pd.DataFrame()
-                for outcome_col in outcome_cols:
-                    row = prev_data[prev_data.MeasureId == outcome_col]
-                    row = row[row.CityName == city]
-                    df2 = us_df[us_df['Outcome'] == outcome_col]
-                    df2['OutcomePrevalence'] = row.Data_Value.iloc[0]
-                    prevention = df2['Prevention'].iloc[:]
-                    p_list = []
-                    for p in prevention:
-                        prevention_row = prev_data[prev_data.MeasureId == p]
-                        prevention_row = prevention_row[prevention_row.CityName == city]
-                        prevention_prevalence = np.sum(
-                            prevention_row.Data_Value * prevention_row.PopulationCount) / np.sum(
-                            prevention_row.PopulationCount)
-                        p_list.append(prevention_prevalence)
-                    df2['PreventionPrevalence'] = p_list
-                    temp = pd.concat([temp, df2])
-                df = temp
+                # Apply national level analysis result for each city
+                city_prev_data = prev_data[prev_data.CityName == city]
+                df = append_existing_data(national_calc_df, city_prev_data)
                 df["Region"] = city
                 df['Level'] = 'City'
                 city_df = pd.concat([city_df, df])
+
+                # Apply state level analysis result for each city if exists
+                if state_calc_df.shape[0] > 0:
+                    df = append_existing_data(state_calc_df, city_prev_data)
+                    df["Region"] = city
+                    df['Level'] = 'City'
+                    city_df = pd.concat([city_df, df])
             result = pd.concat([result, city_df])
         result = pd.concat([result, state_df])
 
         logging.info("Complete data prep in State level")
 
-        result.to_csv("visual_input_tract_with_city.csv")
+        result.to_csv("visual_input_tract_with_city.csv", index=False)
         logging.info("csv file for visualization has been generated")
+    elif mode == 3:
+        print("You have choose to run multiple models comparision analysis.\n")
+        level = input("Enter the level of analysis you want to perform (State/US):\n")
+        if level == 'US':
+            print("About to analysis on national level")
+            multi_model_analysis(city_pv)
+        elif level == 'State':
+            show_state = input("Do you wish to see all the state names? (Y/N)")
+            if show_state == 'Y':
+                print( np.unique(tract_pv['StateDesc']) )
+
+            state = input("Enter the name of the state you want to analyze:\n")
+            state_data = tract_pv[tract_pv['StateDesc'] == state]
+            if state_data.shape[0] == 0:
+                print("Bad state name, abort!")
+                sys.exit()
+            print("About to run model selection for state: ", state, " with ", len(state_data), " census track.")
+            multi_model_analysis(state_data)
+        else:
+            print("Unacceptable input, abort!")
 
 
-# analyze_state_level_data can be used to run different models for all the states
-# analyze_state_level_data(tract_pv)
-
-
-# Below code is the initial version to run different models for US given city level data
-# TODO: make it to an util function
-# clean_data = city_pv
-# x_data = clean_data.loc[:, clean_data.columns.isin(prevention_cols + behavior_cols)]
-#
-# i = 0
-# for outcome_col in outcome_cols:
-#     print("-----------------------------------")
-#     print("#", i)
-#
-#     print("Health Outcome to analyze: ", outcome_col)
-#     i += 1
-#     y_data = clean_data.loc[:, outcome_col]
-#
-#     # split data to training and testing set
-#     x_train, x_test, y_train, y_test = train_test_split(x_data, y_data, train_size=0.7, random_state=random_state)
-#
-#     # TODO: seperate them into a function
-#     # Linear Regression
-#     reg = LinearRegression().fit(x_train, y_train)
-#     # print(reg.intercept_)
-#     # print(reg.coef_)
-#     print(reg)
-#     print("lm train score:", round(reg.score(x_train, y_train), 4))
-#     print("lm test score:", round(reg.score(x_test, y_test), 4))
-#
-#     X = sm.add_constant(x_train)
-#     lm = sm.OLS(y_train, X).fit()
-#     print(lm.summary())
-#
-#     # Ridge Regression
-#     reg = Ridge().fit(x_train, y_train)
-#     print(reg.intercept_)
-#     print(reg.coef_)
-#     print(reg)
-#     print("Ridge train score:", reg.score(x_train, y_train))
-#     print("Ridge test score:", reg.score(x_test, y_test))
-#
-#     # Lasso Regression
-#     reg = Lasso().fit(x_train, y_train)
-#     print(reg.intercept_)
-#     print(reg.coef_)
-#     print(reg)
-#     print("Lasso train score:", reg.score(x_train, y_train))
-#     print("Lasso test score:", reg.score(x_test, y_test))
-#
-#     # SVM - regression with Hyper-parameter Tuning
-#     svr = SVR()
-#     # clf = svr.fit(x_train, y_train)
-#     parameters = {'kernel': ('linear', 'rbf'), 'C': [0.01, 1, 10]}
-#     clf = GridSearchCV(svr, parameters, cv=10).fit(x_train, y_train)
-#     best_param = clf.best_params_
-#     print("svm best params:", clf.best_params_)
-#     print("svm best score:", clf.best_score_)
-#     print("svm best set train accuracy:", round(r2_score(y_train, clf.predict(x_train)), 2))
-#     print("svm best set test accuracy:", round(r2_score(y_test, clf.predict(x_test)), 2))
-#     # print(clf.cv_results_)
-#
-#     # feature selection - recursive feature elimination
-#     estimator = SVR(kernel=best_param['kernel'], C=best_param['C'])
-#     selector = RFE(estimator, 5, step=1)  # top 5 features
-#     selector = selector.fit(x_train, y_train)
-#     # print(x_test.columns)
-#     # print(selector.support_)
-#     # print(selector.ranking_)
-#     print("top features:", x_test.columns[selector.support_])
-
-# TODO: Random Forest Regression?
-
-# TODO: model comparison - select the best model based on score?
+main()
--- util.py
+++ util.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 from sklearn.feature_selection import RFE
-from sklearn.linear_model import LinearRegression, Lasso
+from sklearn.linear_model import LinearRegression, Lasso, Ridge
 from sklearn.metrics import r2_score
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.preprocessing import StandardScaler
@@ -10,43 +10,64 @@ from sklearn.svm import SVR
 from constants import *
 
 
-def analyze_state_level_data(tract_pv):
-    states = np.unique(tract_pv['StateDesc'])
-    for state in states:
-        state_data = tract_pv[tract_pv['StateDesc'] == state]
-        if len(state_data) > 100:
-            print("Run model selection for state: ", state, " with ", len(state_data), " census track.")
-            # split data to training and testing set
-            clean_data = state_data
-            x_data = clean_data.loc[:, clean_data.columns.isin(prevention_cols + behavior_cols)]
-            outcome_col = outcome_cols[0]
-            y_data = clean_data.loc[:, outcome_col]
-            x_train, x_test, y_train, y_test = train_test_split(x_data, y_data, train_size=0.7,
-                                                                random_state=random_state)
+def multi_model_analysis(clean_data):
+    # split data to training and testing set
+    x_data = clean_data.loc[:, clean_data.columns.isin(prevention_cols + behavior_cols)]
+    i = 0
+    for outcome_col in outcome_cols:
+        print("-----------------------------------")
+        print("#", i)
 
-            reg = LinearRegression().fit(x_train, y_train)
-            print("lm coef:", reg.coef_)
-            print("lm train score:", round(reg.score(x_train, y_train), 4))
-            print("lm test score:", round(reg.score(x_test, y_test), 4))
+        print("Health Outcome to analyze: ", outcome_col)
+        i += 1
 
-            # Lasso Regression
-            reg = Lasso().fit(x_train, y_train)
-            print("Lasso coef:", reg.coef_)
-            print("Lasso train score:", round(reg.score(x_train, y_train), 4))
-            print("Lasso test score:", round(reg.score(x_test, y_test), 4))
+        y_data = clean_data.loc[:, outcome_col]
+        # split data to training and testing set
+        x_train, x_test, y_train, y_test = train_test_split(x_data, y_data, train_size=0.7,
+                                                            random_state=random_state)
 
-            # SVM - regression with Hyper-parameter Tuning
-            svr = SVR()
-            parameters = {'kernel': ('linear', 'rbf'), 'C': [0.01, 1, 10]}
-            clf = GridSearchCV(svr, parameters, cv=10).fit(x_train, y_train)
-            best_param = clf.best_params_
-            print("svm best set train accuracy:", round(r2_score(y_train, clf.predict(x_train)), 4))
-            print("svm best set test accuracy:", round(r2_score(y_test, clf.predict(x_test)), 4))
-            # feature selection - recursive feature elimination
-            estimator = SVR(kernel=best_param['kernel'], C=best_param['C'])
-            selector = RFE(estimator, 5, step=1)  # top 5 features
-            selector = selector.fit(x_train, y_train)
-            print("top features:", x_test.columns[selector.support_])
+        # Linear Regression
+        reg = LinearRegression().fit(x_train, y_train)
+        # print(reg.intercept_)
+        print("lm coef:", reg.coef_)
+        print("lm train score:", round(reg.score(x_train, y_train), 4))
+        print("lm test score:", round(reg.score(x_test, y_test), 4))
+
+        #     X = sm.add_constant(x_train)
+        #     lm = sm.OLS(y_train, X).fit()
+        #     print(lm.summary())
+
+        # Ridge Regression
+        reg = Ridge().fit(x_train, y_train)
+        print(reg.intercept_)
+        print(reg.coef_)
+        print(reg)
+        print("Ridge train score:", reg.score(x_train, y_train))
+        print("Ridge test score:", reg.score(x_test, y_test))
+
+        # Lasso Regression
+        reg = Lasso().fit(x_train, y_train)
+        print("Lasso coef:", reg.coef_)
+        print("Lasso train score:", round(reg.score(x_train, y_train), 4))
+        print("Lasso test score:", round(reg.score(x_test, y_test), 4))
+
+        # SVM - regression with Hyper-parameter Tuning
+        svr = SVR()
+        parameters = {'kernel': ('linear', 'rbf'), 'C': [0.01, 1, 10]}
+        clf = GridSearchCV(svr, parameters, cv=10).fit(x_train, y_train)
+        best_param = clf.best_params_
+        print("svm best params:", clf.best_params_)
+        print("svm best score:", clf.best_score_)
+        print("svm best set train accuracy:", round(r2_score(y_train, clf.predict(x_train)), 4))
+        print("svm best set test accuracy:", round(r2_score(y_test, clf.predict(x_test)), 4))
+        # feature selection - recursive feature elimination
+        estimator = SVR(kernel=best_param['kernel'], C=best_param['C'])
+        selector = RFE(estimator, 5, step=1)  # top 5 features
+        selector = selector.fit(x_train, y_train)
+        # print(x_test.columns)
+        # print(selector.support_)
+        # print(selector.ranking_)
+        print("top features:", x_test.columns[selector.support_])
 
 
 # Top n Feature selection on given data for the specified columns
@@ -121,7 +142,7 @@ def visual_data_prep(clean_data, prev_data, cols, n=5):
     return df
 
 
-def append_existing_data(df, prev_data, region):
+def append_existing_data(df, prev_data):
     temp = pd.DataFrame()
     for outcome_col in outcome_cols:
         outcome_row = prev_data[prev_data.MeasureId == outcome_col]
@@ -141,12 +162,10 @@ def append_existing_data(df, prev_data, region):
                 prevention_row.PopulationCount)
             p_list.append(prevention_prevalence)
 
-        df2['Prevention Prevalence'] = p_list
+        df2['PreventionPrevalence'] = p_list
         temp = pd.concat([temp, df2])
-    df = temp
-    df["Region"] = region
-    df['Level'] = 'State'
-    df['PreventionCalculationLevel'] = 'National'
+    return temp
+
 
 # Region	Level	Outcome	Prevention	Outcome Prevelance	Prevention Prevelence	Prevention Rank	Prevention Score
 # US	US	Arthritis	Health Insurance	25.4	11.6	1	0.509765339
